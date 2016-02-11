29805d9c1262771c16839cc7a4ecc57bbd72e940
Frank.YW Chen(陳衍文) <frank.yw_chen@htc.com>
2016-02-11 11:38:28 +0800

    wrap as class

--- pla.py
+++ pla.py
@@ -3,26 +3,34 @@ import matplotlib
 import matplotlib.pyplot as plt
 import data_generator as dg
 
-# 1x + 1y + 0 = 0
+class PLA:
+  def __init__(self):
+    self.init = 0
+  def DataGeneration(self, n, dim, upper, lower, ref):
+    i = 0;
+    while i < n:
+      dum = dg.Data(dg.Dimension(dim, upper, lower))
+      dum.SetDataLabel(np.sign(np.dot(dum.value_, ref)))
+      yield dum
+      i = i + 1
+  def PlotDataHandle(self, handle):
+    plt.figure()
+    for data in handle:
+      if data.label_ > 0:
+        plt.plot( data.value_[0], data.value_[1], 'b.')
+      else:
+        plt.plot( data.value_[0], data.value_[1], 'r.')
+    plt.title('x vs y')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+    #plt.savefig('sp_vs_sl_{}.png'.format(name))
+  def test(self):
+    DataHandle = self.DataGeneration(20, 2, 10, -10, np.array([1 ,2 ,0]))
+    self.PlotDataHandle(DataHandle)
 
-def DataGeneration(n, dim, upper, lower, ref):
-  i = 0;
-  while i < n:
-    dum = dg.Data(dg.Dimension(dim, upper, lower))
-    dum.SetDataLabel(np.sign(np.dot(dum.value_, ref)))
-    yield dum
-    i = i + 1
-
-DataHandel = DataGeneration(100, 2, 10, -10, np.array([1 ,1 ,0]))
-plt.figure()
-for data in DataHandel:
-  if data.label_ > 0:
-    plt.plot( data.value_[0], data.value_[1], 'b.')
-  else:
-    plt.plot( data.value_[0], data.value_[1], 'r.')
-
-plt.title('x vs y')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
-#plt.savefig('sp_vs_sl_{}.png'.format(name))
+########
+# test #
+########
+pla = PLA()
+pla.test()
